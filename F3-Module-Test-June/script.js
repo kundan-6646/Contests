11810b7f1b25386b7af6c6db5909567908bd627b
unknown <kundanjha826@gmail.com>
2023-07-04 23:14:50 +0530

    contest-f3-contest-added

--- F3-Module-Test-June/script.js
+++ F3-Module-Test-June/script.js
@@ -1,12 +1,39 @@
-// myProducts.filter((item)=>item.title.includes(search.value))
+let geoData;
 
-// myCartProductArray = myProducts.filter((item)=> myCartIDs.includes(item.id))
-
-if(!localStorage.getItem("users")) {
-    const userArr = [];
-    localStorage.setItem("users", JSON.stringify({userArr}));
+//async method to get Geo data
+async function fetchGeoLocationByIP() {
+    try {
+      const response = await fetch(`https://ip-api.io/api/json`);
+      const data = await response.json();
+      geoData = data;
+      renderData();
+    } catch (error) {
+      console.log('Error:', error);
+    }
 }
 
-if(!localStorage.getItem("loggedInUser")) {
-    localStorage.setItem("loggedInUser", JSON.stringify({}));
+//This object is a collection of keys that we going to get from our geoData
+//we will iterate over this map to get create Ui ele and their val we use in to get geodata.keyname
+const reqData = {
+    Lat: "latitude",
+    City: "city",
+    Organisation: "organisation",
+    Long: "longitude",
+    Region: "region_name",
+    Hostname: "ip"
 }
+
+const geoDataContainer = document.getElementsByClassName('geo-data-container')[0];
+function renderData() {
+    geoDataContainer.innerHTML = '';
+    for (const key in reqData) {
+        const keyForGeoData = reqData[key];
+        
+        let div = document.createElement('div');
+        div.classList.add('geo-data-item');
+        div.innerHTML = `${key}: <span>${geoData[keyForGeoData]}</span>`;
+
+        geoDataContainer.appendChild(div);
+    }
+}
+  
